a96ba5b82d2b57b7b842e38568abb982c7d3efd8
Tossawat <51531950+tanza4432@users.noreply.github.com>
2022-03-01 19:48:21 +0700

    update

--- src/page/Gallery/index.tsx
+++ src/page/Gallery/index.tsx
@@ -1,8 +1,20 @@
 import "./gallery.css";
 
+
 export default function Gallery() {
   return (
     <>
+    <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+  <Tab eventKey="home" title="Home">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="profile" title="Profile">
+    <Sonnet />
+  </Tab>
+  <Tab eventKey="contact" title="Contact" disabled>
+    <Sonnet />
+  </Tab>
+</Tabs>
       <ul className="nav nav-tabs" id="myTab" role="tablist">
         <li className="nav-item" role="presentation">
           <button
