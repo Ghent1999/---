548d2554a6a7ac5af4b0fa973da90ac25e4da94d
Tossawat <51531950+tanza4432@users.noreply.github.com>
2022-03-05 11:33:38 +0700

    Up

--- src/services/GalleryService.ts
+++ src/services/GalleryService.ts
@@ -41,35 +41,11 @@ const uploadImageGallery = (data: File) => {
   });
 };
 
-const get = (id: any) => {
-  return httpClient.get<GalleryModel>(`/tutorials/${id}`);
-};
-const create = (data: GalleryModel) => {
-  return httpClient.post<GalleryModel>("/tutorials", data);
-};
-const update = (id: any, data: GalleryModel) => {
-  return httpClient.put<any>(`/tutorials/${id}`, data);
-};
-const remove = (id: any) => {
-  return httpClient.delete<any>(`/tutorials/${id}`);
-};
-const removeAll = () => {
-  return httpClient.delete<any>(`/tutorials`);
-};
-const findByTitle = (title: string) => {
-  return httpClient.get<Array<GalleryModel>>(`/tutorials?title=${title}`);
-};
 const TutorialService = {
   getAllGallery,
   getAllGallerySort,
   getLastNo,
   addGallery,
   uploadImageGallery,
-  get,
-  create,
-  update,
-  remove,
-  removeAll,
-  findByTitle,
 };
 export default TutorialService;
