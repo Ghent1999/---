6bdc0db989718a68316dbc304e7a1f74286dadfc
กีตาร์เอาเอง <guitar@odds.team>
2022-04-09 19:13:52 +0700

    :guitar:

--- src/page/history-learning/index.tsx
+++ src/page/history-learning/index.tsx
@@ -134,15 +134,7 @@ export default function HistoryLearning() {
                       </h3>
                     </div>
                     <div className="card-body">
-                      <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d246574.9540918541!2d100.14244100000002!3d15.062841!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e1910f215b5aaf%3A0x6db855308e4d8df1!2z4Lih4Lir4Liy4Lin4Li04LiX4Lii4Liy4Lil4Lix4Lii4Lij4Liy4LiK4Lig4Lix4LiP4LiI4Lix4LiZ4LiX4Lij4LmA4LiB4Lip4LihLeC4iuC4seC4ouC4meC4suC4lw!5e0!3m2!1sth!2sth!4v1645991070715!5m2!1sth!2sth"
-                        width={600}
-                        height={450}
-                        style={{ border: 0 }}
-                        allowFullScreen
-                        loading="lazy"
-                        title="Map"
-                      />
+                      <img src="assets/images/history-learning.png" alt="" />
                     </div>
                   </div>
                 </div>
