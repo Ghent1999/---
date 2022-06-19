f9d0e166f6a33d73b4b4d3ec0affcc7fd5f4f95b
กีตาร์เอาเอง <guitar@odds.team>
2022-06-19 22:18:53 +0700

    ทม

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Gallery from "./page/Gallery";
 import ContactPage from "./page/Contact/ContactPage";
 import Person from "./page/Person";
 import HistoryLearning from "./page/history-learning";
+import LoginPage from "./page/Login";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/contact" element={<ContactPage />}></Route>
             <Route path="/data-image" element={<DataImage />}></Route>
             <Route path="/gallery" element={<Gallery />}></Route>
+            <Route path="/login" element={<LoginPage />}></Route>
             <Route
               path="/history-learning"
               element={<HistoryLearning />}
